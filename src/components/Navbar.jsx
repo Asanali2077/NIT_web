1eaf5b7a9c9f834bfc4364286edabe36a4558ed2
adjaragoodju <adilkrokodil05@gmail.com>
2025-06-17 11:07:02 +0500

    improved navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,8 +4,6 @@ import sunIcon from '../assets/images/sunicon2.svg';
 import moonIcon from '../assets/images/sunicon1.svg';
 import chevronDownIcon from '../assets/images/darkicon.svg';
 import chevronDownIconLight from '../assets/images/lighticon.svg';
-import exitToAppIcon from '../assets/images/Exit to app.png';
-import searchIcon from '../assets/images/Search.png';
 import whiteLogo from '../assets/images/light.svg';
 import blackLogo from '../assets/images/dark.svg';
 import { useLanguage } from '../context/LanguageContext';
@@ -56,10 +54,6 @@ function Navbar() {
           </div>
         </div>
         <div className="navbar-right">
-          <div className="search-container">
-            <input type="text" placeholder={t('search')} className="search-input" />
-            <img src={searchIcon} alt="Search" className="search-icon" />
-          </div>
           <div className="theme-toggle" onClick={toggleTheme}>
             <img src={isDarkTheme ? sunIcon : moonIcon} alt="Theme" className="theme-icon" />
           </div>
@@ -74,17 +68,12 @@ function Navbar() {
               </div>
             )}
           </div>
-          <button className="login-button">
-            <img src={exitToAppIcon} alt="Login" className="user-icon" />
-            <span>{t('personal-cabinet')}</span>
-          </button>
         </div>
       </div>
       <div className="navbar-menu">
         <button className="menu-item active">{t('developments')}</button>
         <button className="menu-item">{t('achievements')}</button>
         <button className="menu-item">{t('submit-request')}</button>
-        <button className="menu-item">{t('ccs')}</button>
       </div>
     </div>
   );
